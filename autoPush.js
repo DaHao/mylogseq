4c05925edfdf866152c861637bde6d400912534e
Hao <jiash561@gmail.com>
2022-06-06 09:57:19 +0800

    Auto saved by Logseq

--- autoPush.js
+++ autoPush.js
@@ -1,18 +1,17 @@
 const { exec } = require("child_process");
 
 function executeCmd(cmd) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) return reject(error);
+      return resolve({ stdout, stderr });
+    });
+  });
 }
 
-exec("git diff origin/master..HEAD", (error, stdout, stderr) => {
-  if (error) {
-    console.log('Error:', error);
-    return;
-  }
-
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-
-  console.log(`stdout: ${stdout === ''}`);
-});
+executeCmd("git diff origin/master..HEAD")
+  .then(({ stdout, stderr }) => {
+    console.log('stderr: ', stderr);
+    console.log('stdout', stdout);
+  })
+  .catch(err => { console.log('Error:', error); });
---
+++ logseq/bak/autoPush/2022-06-06T01_55_11.107Z.js
@@ -0,0 +1,24 @@
+const { exec } = require("child_process");
+
+function executeCmd(cmd) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) { reject(error); return; }
+      return resolve({ stdout, stderr });
+    });
+  });
+}
+
+exec("git diff origin/master..HEAD", (error, stdout, stderr) => {
+  if (error) {
+    console.log('Error:', error);
+    return;
+  }
+
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+
+  console.log(`stdout: ${stdout === ''}`);
+});
